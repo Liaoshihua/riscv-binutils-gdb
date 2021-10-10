cc1c46ef44ff49902a6b7c61765fd7d35f2201a2
Lin Sinan <sinan.lin@nokia.com>
2021-10-10 15:20:34 +0300

    update testcase for decbnez code gen

--- gas/testsuite/gas/riscv/insn-zceb-64.d
+++ gas/testsuite/gas/riscv/insn-zceb-64.d
@@ -10,3 +10,4 @@ Disassembly of section .text:
 0+000 <zceb64>:
 [	 ]+0:[	 ]+40803007[	 ]+ld[	 ]+s0,1(gp)
 [	 ]+4:[	 ]+40003427[	 ]+sd[	 ]+s0,1(gp)
+[	 ]+8:[	 ]+804a3507[	 ]+decbnez[	 ]+a0,4,c <.L2>
--- gas/testsuite/gas/riscv/insn-zceb-64.s
+++ gas/testsuite/gas/riscv/insn-zceb-64.s
@@ -1,3 +1,8 @@
 zceb64:
 	ld s0,1(gp)
 	sd s0,1(gp)
+	addi a0,a0,-4
+	bne  a0,zero,.L2
+.L2:
+	sext.w	a0,a0
+	ret
