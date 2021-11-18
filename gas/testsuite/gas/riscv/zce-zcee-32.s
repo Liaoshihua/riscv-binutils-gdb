8820df496f2fbd388b2ad4450e9260c8d3979232
LiaoShihua <shihua@iscas.ac.cn>
2021-11-18 15:31:36 +0800

    add zcee's testcase

---
+++ gas/testsuite/gas/riscv/zce-zcee-32.s
@@ -0,0 +1,13 @@
+zcee32:
+	c.zext.b a5
+	c.zext.h a5
+	c.sext.b a5
+	c.sext.h a5
+	zext.b   a5, a5
+	zext.h   a5, a5
+	sext.b   a5, a5
+	sext.h   a5, a5
+	mul      a6, a6, a7
+	mul      a3, a3, a5
+	c.mul    a3, a5
+	andi a5,a5,0xff
